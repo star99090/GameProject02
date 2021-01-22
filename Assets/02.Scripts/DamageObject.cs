ff7a91f6547044d8997e923127f27c1ab6814e83
star99090 <star99090@naver.com>
2021-01-22 23:51:56 +0900

    라인트랩, 언덕 생성

---
+++ Assets/02.Scripts/DamageObject.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageObject : MonoBehaviour
+{
+    bool isFirst = true;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player" && isFirst == true)
+        {
+            Debug.Log("Damaged");
+            isFirst = false;
+        }
+    }
+}
---
+++ Assets/02.Scripts/LineTrab.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class LineTrab : MonoBehaviour
+{
+    public GameObject trab;
+    Rigidbody rigid;
+
+    private void Awake()
+    {
+        rigid = trab.gameObject.GetComponent<Rigidbody>();
+    }
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        rigid.isKinematic = false;
+        Destroy(this.gameObject);
+    }
+}
