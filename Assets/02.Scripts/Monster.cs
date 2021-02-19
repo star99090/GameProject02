9876aac2097c9a4a424a65ac74e24751c3eca2ca
star99090 <star99090@naver.com>
2021-02-19 16:57:33 +0900

    몬스터 스크립트 작성 중

---
+++ Assets/02.Scripts/Monster.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster : MonoBehaviour
+{
+    public GameObject target;
+    private bool isTrace;
+    private bool isStart;
+    
+    private void Update()
+    {
+        if (isStart)
+        {
+            transform.position = Vector3.Lerp(transform.position, new Vector3(target.transform.position.x, transform.position.y, target.transform.position.z), Time.deltaTime);
+            if (Vector3.Distance(target.transform.position, transform.position) <= 0.5f)
+            {
+                isTrace = false;
+            }
+            else
+            {
+                isTrace = true;
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            isStart = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            isStart = false;
+        }
+    }
+}
